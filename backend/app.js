fbfabe19ae71e9b405cb392dd457494e6d989a39
“xXFiEsTaDeAmOnXx” <niklas.kleiser@web.de>
2022-11-07 13:25:23 +0100

    Implemented mysql database container and database connection

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,37 @@
 const express = require('express');
+const mysql = require('mysql');
+
+require("dotenv").config(); //load environment variables
+
 const app = express();
 const PORT = 8081;
 
+let database = mysql.createConnection({
+  host:  process.env.DB_HOST,
+  user:  process.env.DB_USER,
+  password:  process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+database.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  console.log('Connected to the MySQL server.');
+});
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  const sqlQuery =  'CREATE TABLE Auto (Bezeichnung VARCHAR(50));';
+
+    database.query(sqlQuery, (err) => {
+        if (err) throw err;
+
+    res.send('Table created!');
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
-})
+});
